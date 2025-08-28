1df5305deac5eb0532d092e984a539e56b561090
Phuong (Julia) <159302954+phuongwj@users.noreply.github.com>
2025-08-28 20:27:52 -0300

    Time: 3 ms (22.89%) | Memory: 17.9 MB (32.33%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.py
+++ 20-valid-parentheses/valid-parentheses.py
@@ -6,7 +6,9 @@ class Solution:
             if s[i] == '(' or s[i] == '{' or s[i] == '[':
                 stack.append(s[i])
             elif len(stack) != 0:
-                if (s[i] == ')' and stack[-1] == '(') or (s[i] == '}' and stack[-1] == '{') or (s[i] == ']' and stack[-1] == '['):
+                if (s[i] == ')' and stack[-1] == '(') \
+                or (s[i] == '}' and stack[-1] == '{') \
+                or (s[i] == ']' and stack[-1] == '['):
                     stack.pop() 
                 else:
                     return False
