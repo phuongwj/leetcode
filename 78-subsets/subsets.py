96a132b74f87120701c27cbfc927eebe15056d6e
Phuong (Julia) <159302954+phuongwj@users.noreply.github.com>
2026-08-03 01:39:46 -0300

    Time: 0 ms (100.00%) | Memory: 19.4 MB (66.92%) - LeetSync

---
+++ 78-subsets/subsets.py
@@ -0,0 +1,30 @@
+class Solution:
+    def subsets(self, nums: List[int]) -> List[List[int]]:
+
+        """
+        backtracking, recursion.
+        we have two choices for each number.
+        """
+        
+        allSubsets = []
+
+        def findSubsets(currSubset, curr) -> List[List[int]]:
+            # we've reached the last element if index is out of bounds
+            if curr == len(nums):
+                copy = currSubset.copy()
+                allSubsets.append(copy)
+                return allSubsets
+
+            # include branch.
+            currSubset.append(nums[curr])
+            findSubsets(currSubset, curr + 1)
+
+            # exclude branch. note: if you exclude it, you must pop the 
+            # most recent one so you can add the one after it.
+            currSubset.pop()
+            findSubsets(currSubset, curr + 1)
+            
+
+        # gives first index
+        findSubsets([], 0)
+        return allSubsets
