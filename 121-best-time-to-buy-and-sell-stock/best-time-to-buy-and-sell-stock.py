03ef9e724bb098f880a88027171f20f0de9775fb
Phuong (Julia) <159302954+phuongwj@users.noreply.github.com>
2026-08-15 11:45:41 -0300

    Time: 59 ms (33.12%) | Memory: 28.6 MB (75.59%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,20 +1,21 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
+        '''
+        brute-force solution: go through every single price, check the prices in the
+        later days to see whether it would give us the maximum comparing to the current
+        maximum we have, if yes then we replace that maximum. do it until we reach end
+        of array. -> O(n^2)
+        '''
+
         n = len(prices)
 
-        if n <= 1:
-            return 0
+        maxProfit = 0
+        minCost = prices[0]
 
-        buy = 0
-        sell = 1
-        profit = 0
+        for i in range(n):
+            minCost = min(minCost, prices[i])
+            temp = prices[i] - minCost
+            if temp > maxProfit:
+                maxProfit = temp
 
-        while sell < n:
-
-            if prices[buy] > prices[sell]:
-                buy = sell
-
-            profit = max(profit, prices[sell] - prices[buy])
-            sell += 1
-
-        return profit
+        return maxProfit
