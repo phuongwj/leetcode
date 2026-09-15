4fa448bc3a2c8c7a668e4b79917fbe58438f162f
Phuong (Julia) <159302954+phuongwj@users.noreply.github.com>
2026-09-14 23:15:30 -0300

    Time: 4 ms (13.50%) | Memory: 19.3 MB (29.27%) - LeetSync

--- 1391-counting-elements/counting-elements.py
+++ 1391-counting-elements/counting-elements.py
@@ -1,15 +1,24 @@
 class Solution:
     def countElements(self, arr: List[int]) -> int:
         """
-        Constraints is small enough for the O(n^2) brute-force solution to pass
+        Constraints are small enough for the O(n^2) brute-force solution to pass
+
+        Can use hash set to store all numbers in arr, which gives us O(1) lookup.
+        Time complexity: O(n) and Space Complexity O(n)
         """
 
-        counter = 0
+        storage = set()
         n = len(arr)
 
         for i in range(n):
+            if arr[i] not in storage:
+                storage.add(arr[i])
+
+        counter = 0
+
+        for i in range(n):
             extra = arr[i] + 1
-            if extra in arr:
+            if extra in storage:
                 counter += 1
 
         return counter
