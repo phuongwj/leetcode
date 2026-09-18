182353a13b0c1ee5f065418f4d723f7c5eee3af8
Phuong (Julia) <159302954+phuongwj@users.noreply.github.com>
2026-09-18 18:47:29 -0300

    Time: 3 ms (21.17%) | Memory: 19.3 MB (73.71%) - LeetSync

--- 14-longest-common-prefix/longest-common-prefix.py
+++ 14-longest-common-prefix/longest-common-prefix.py
@@ -1,27 +1,19 @@
 class Solution:
-    def longestCommonPrefix(self, strs: List[str]) -> str:
-        
-        shortestStr = min(strs, key=len)
-        
+    def longestCommonPrefix(self, strs: list[str]) -> str:
+        # the whole array needs to have the same prefix string
 
-        # Start with the shortest string
-        # For every single prefix of the shortest string, we
-        # check if the other strings start with the prefix
-        # if we found a prefix that doesn't match for any string, 
-        # then the previous prefix is our answer 
+        n = len(strs[0])
+        first = strs[0]
+        prefix = ""
 
         if len(strs) == 1:
             return strs[0]
-        elif len(strs) == 0:
-            return ""
 
-        prefix = ""
-
-        for i in range(1, len(shortestStr) + 1):
-            prefix = shortestStr[:i]
+        for i in range(1, n+1):
+            prefix = first[:i]
 
             for string in strs:
                 if not string.startswith(prefix):
-                    return shortestStr[:i-1]
+                    return prefix[:-1]
 
         return prefix
